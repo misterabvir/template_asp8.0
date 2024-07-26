f836b052888e102c21aaa448c244db26d113e059
misterabvir <to@misterabvir.ru>
2024-07-26 10:23:44 +0300

    added separated configuration files

--- src/AuthenticationService/Presentation/Program.cs
+++ src/AuthenticationService/Presentation/Program.cs
@@ -5,6 +5,16 @@ using Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// TODO : Replace this on user secrets
+builder.Configuration.AddJsonFile("Settings/appsettings.Secrets.json", optional: false, reloadOnChange: true);
+
+// configuration
+builder.Configuration.AddJsonFile("Settings/cleaner-settings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("Settings/encrypt-settings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("Settings/outbox-settings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("Settings/token-settings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("Settings/verification-settings.json", optional: false, reloadOnChange: true);
+
 builder.Services
     .AddInfrastructure(builder.Configuration)
     .AddPersistence(builder.Configuration)
@@ -12,3 +22,4 @@ builder.Services
     .AddPresentation(builder.Configuration);
 
 builder.Build().UsePresentation().Run();
+
